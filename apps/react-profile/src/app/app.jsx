8754cd7e7b2c1d140ee6e58cec5ab4bdddeba03b
milad <milad.rastgoou@gmail.com>
2023-07-24 17:00:12 +0330

    first commit

---
+++ apps/react-profile/src/app/app.jsx
@@ -0,0 +1,10 @@
+import NxWelcome from './nx-welcome';
+export function App() {
+  return (
+    <div>
+      <NxWelcome />
+      <h1>salam</h1>
+    </div>
+  );
+}
+export default App;
---
+++ apps/react-provider/src/app/app.jsx
@@ -0,0 +1,9 @@
+import NxWelcome from './nx-welcome';
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-provider" />
+    </div>
+  );
+}
+export default App;
